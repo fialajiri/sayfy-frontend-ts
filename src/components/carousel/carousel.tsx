823ff5ce3922c0ccfbb99c510cae28be5e6285dc
Jiri Fiala <jirik.fiala@post.cz>
2022-09-16 14:35:12 +0200

    small fix

--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -3,19 +3,19 @@ import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 
 const newsData = [
-  <div>
+  <div data-value="1" key={1}>
     <p>This is news 1</p>
   </div>,
-  <div>
+  <div data-value="2" key={2}>
     <p>This is news 2</p>
   </div>,
-  <div>
+  <div data-value="3" key={3}>
     <p>This is news 3</p>
+  </div >,
+  <div data-value="4" key={4}>
+    <p>This is news 4</p>
   </div>,
-  <div>
-    <p>This is news 5</p>
-  </div>,
-  <div>
+  <div data-value="5" key={5}>
     <p>This is news 5</p>
   </div>,
 ];
@@ -27,7 +27,7 @@ const responsive = {
 };
 
 const Carousel: React.FC = () => {
-  return <AliceCarousel mouseTracking items={newsData} controlsStrategy="alternate" responsive={responsive} />;
+  return <AliceCarousel mouseTracking items={newsData} controlsStrategy="alternate" responsive={responsive}  />;
 };
 
 export default Carousel;
