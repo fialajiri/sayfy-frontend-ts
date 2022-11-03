5e860b5f7f1f78f80f756c04e5db9ad315dfa85c
fialajiri <jirik.fiala@post.cz>
2022-11-03 20:30:54 +0100

    image resize

--- src/components/ui-elements/image-card.tsx
+++ src/components/ui-elements/image-card.tsx
@@ -8,12 +8,14 @@ interface ImageCardProps {
 
 const ImageCard: React.FC<ImageCardProps> = ({ imageUrl, title, alt }) => {
   const cloudFormationUrl = `https://d3o6i10cj9gxf0.cloudfront.net`;
-  const filter = `fit-in/800x540`;
+  const filter = `fit-in/600x400`;
   const imagePath = imageUrl.includes("https")
     ? imageUrl.replace(process.env.IMAGE_DOMAIN as string, "")
     : imageUrl;
 
   const imageFinalUrl = `${cloudFormationUrl}/${filter}/${imagePath}`;
+
+  console.log(imageFinalUrl)
 
   return (
     <div className="image-card__container">
