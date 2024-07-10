4249d020cca517c9c151da044fc3fd11d52a145e
fialajiri <jirik.fiala@post.cz>
2024-07-10 09:51:48 +0200

    feat: register button href update

--- src/components/ui-elements/register-button.tsx
+++ src/components/ui-elements/register-button.tsx
@@ -3,7 +3,7 @@ import Button from "./button";
 const RegistrButton: React.FC = () => {
   return (
     <div className="register-button">
-      <a className="register-button__anchor" href="https://sportt.cz/register/1278" target='_blank' rel="noreferrer">
+      <a className="register-button__anchor" href="https://sportt.cz/register/1365" target='_blank' rel="noreferrer">
         Registrace Zde
       </a>
     </div>
