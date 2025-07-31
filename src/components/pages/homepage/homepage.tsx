72a81b1837c7d29c222768d0fa0a898ee2f5e985
fialajiri <jirik.fiala@post.cz>
2025-07-31 20:38:43 +0200

    feat: added logos

--- src/components/pages/homepage/homepage.tsx
+++ src/components/pages/homepage/homepage.tsx
@@ -3,17 +3,16 @@ import NewsSection from "./news-section";
 import InfoSection from "./info-section";
 import { AktualitaDoc } from "../../../models/models";
 import { Fragment } from "react";
-// import RegistrButton from "../../ui-elements/register-button";
-
+import RegistrButton from "../../ui-elements/register-button";
 
 interface HomePageProps {
-  aktuality: AktualitaDoc[]
+  aktuality: AktualitaDoc[];
 }
 
-const HomePage: React.FC<HomePageProps> = ({aktuality}) => {
+const HomePage: React.FC<HomePageProps> = ({ aktuality }) => {
   return (
-    <Fragment >
-      {/* <RegistrButton /> */}
+    <Fragment>
+      <RegistrButton />
       <SayfyHero />
       <InfoSection />
       <NewsSection aktuality={aktuality} />
--- src/components/pages/support/support-page.tsx
+++ src/components/pages/support/support-page.tsx
@@ -8,17 +8,22 @@ import Logo6 from "../../../../public/logos/support/6-transparent.png";
 import Logo7 from "../../../../public/logos/support/7-transparent.png";
 import Logo8 from "../../../../public/logos/support/8-transparent.png";
 import Logo9 from "../../../../public/logos/support/9-transparent.png";
+import Logo10 from "../../../../public/logos/support/10-transparent.png";
+import Logo11 from "../../../../public/logos/support/11-transparent.png";
+import Logo12 from "../../../../public/logos/support/12-transparent.png";
 
 const logos = [
-  { src: Logo1, alt: 'image_1' },
-  { src: Logo2, alt: 'image_2' },
-  { src: Logo3, alt: 'image_3' },
-  { src: Logo4, alt: 'image_4' },
-  { src: Logo5, alt: 'image_5' },
-  { src: Logo6, alt: 'image_6' },
-  { src: Logo7, alt: 'image_7' },
-  { src: Logo8, alt: 'image_8' },
-  { src: Logo9, alt: 'image_9' },
+  { src: Logo1, alt: "image_1" },
+  { src: Logo2, alt: "image_2" },
+  { src: Logo4, alt: "image_4" },
+  { src: Logo5, alt: "image_5" },
+  { src: Logo6, alt: "image_6" },
+  { src: Logo7, alt: "image_7" },
+  { src: Logo8, alt: "image_8" },
+  { src: Logo9, alt: "image_9" },
+  { src: Logo10, alt: "image_10" },
+  { src: Logo11, alt: "image_11" },
+  { src: Logo12, alt: "image_12" },
 ];
 
 const SupportPage: React.FC = () => {
@@ -43,4 +48,3 @@ const SupportPage: React.FC = () => {
 };
 
 export default SupportPage;
-
